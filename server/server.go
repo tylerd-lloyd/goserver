aa90ee253b3d77ccabdb3dd6ac58455535069259
Tyler Lloyd <tlloyd9@gatech.edu>
2020-10-28 22:15:54 -0400

    Add server logging

--- server/server.go
+++ server/server.go
@@ -20,14 +20,14 @@ var dataMap map[int]models.Metadata
 var counter = 1
 
 // Run main http server
-func Run() {
-	dataMap = make(map[int]models.Metadata)
+func Run(port string) {
+	dataMap = make(map[int]models.Metadata, 100)
 	router := mux.NewRouter()
-	fmt.Println("Running server at localhost:50001")
 	router.HandleFunc("/api/metadata", createMetadata).Methods("POST")
 	router.HandleFunc("/api/metadata", searchMetadata).Methods("GET")
 	router.HandleFunc("/api/metadata/{id}", getMetadata).Methods("GET")
-	log.Fatal(http.ListenAndServe(":50001", router))
+	fmt.Printf("Server running at localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -41,18 +41,22 @@ func createMetadata(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "error: %v", err)
+		log.Printf("BadRequest 400 - %v\n", err)
 		return
 	}
 
 	if err := utils.ValidateMetadata(t); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, err.Error())
+		log.Printf("BadRequest 400 - %s\n", err.Error())
 		return
 	}
 	t.ID = counter
 	counter++
 	dataMap[t.ID] = t
+	w.WriteHeader(http.StatusCreated)
 	yaml.NewEncoder(w).Encode(t)
+	log.Printf("Created 201 - OK\n")
 }
 
 func getMetadata(w http.ResponseWriter, r *http.Request) {
@@ -62,29 +66,40 @@ func getMetadata(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "error %v", err)
+		log.Printf("BadRequest 400 - %v\n", err)
 		return
 	}
 
 	if m, ok := dataMap[key]; ok {
 		m.ID = 0 // exclude ID field from the response object
 		yaml.NewEncoder(w).Encode(m)
+		log.Printf("Success 200 - OK\n")
 		return
 	}
 
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprintf(w, "")
+	log.Printf("NotFound 404 - id=%d\n", key)
 }
 
 func searchMetadata(w http.ResponseWriter, r *http.Request) {
-	params := r.URL.Query()
-	for p, v := range params {
-		fmt.Printf("%v, %v", p, v)
-	}
-	var sb strings.Builder
+	params := make(map[string][]string)
 
+	for p, v := range r.URL.Query() {
+		params[strings.ToLower(p)] = v
+	}
+
+	filtered := utils.FilterMetadataMap(dataMap, params)
+
+	fmt.Fprint(w, buildResponse(filtered))
+	log.Printf("Success 200 - OK\n")
+}
+
+func buildResponse(data map[int]models.Metadata) string {
+	var itemSize int
+	var sb strings.Builder
 	sb.WriteString("items:\n")
-	itemSize := 0
-	for _, v := range dataMap {
+	for _, v := range data {
 		itemSize++
 		bytes, _ := yaml.Marshal(v)
 		s := string(bytes)
@@ -94,9 +109,12 @@ func searchMetadata(w http.ResponseWriter, r *http.Request) {
 		s = strings.TrimRight(s, "\n    ")
 		s = s + "\n"
 		sb.WriteString(fmt.Sprintf("  -\n    %s", s))
+		if itemSize == 100 {
+			break // limit response size to 100 items
+		}
 	}
 
 	sb.WriteString(fmt.Sprintf("count: %d\n", itemSize))
 
-	fmt.Fprint(w, sb.String())
+	return sb.String()
 }
