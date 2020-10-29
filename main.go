760f0fe08473a696fa12fa2b5f76fc58d7c7cbdc
Tyler Lloyd <tlloyd9@gatech.edu>
2020-10-28 22:15:08 -0400

    Adding cli flags

--- main.go
+++ main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
+	"flag"
 	"restserverfd/server"
-
-	"gopkg.in/yaml.v2"
 )
 
-var data = `
-a: Easy
-b:
-  c: 2
-  d: [3,4]
-`
-
-type T struct {
-	A string
-	B struct {
-		RenamedC int   `yaml:"c"`
-		D        []int `yaml:",flow"`
-	}
-}
-
 func main() {
-	server.Run()
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	t := T{}
-
-	err := yaml.UnmarshalStrict([]byte(data), &t)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "error: %v", err)
-	} else {
-		fmt.Fprintf(w, "Welcome! %v", t)
-	}
+	port := flag.String("p", "50001", "localhost port")
+	flag.Parse()
+	server.Run(*port)
 }
